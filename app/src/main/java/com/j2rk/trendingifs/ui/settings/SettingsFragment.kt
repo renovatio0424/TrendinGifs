4aa4aacfab7360d6ece49554f6c5b0acbaffa5c1
Reno <reno.kim@kinemaster.com>
2021-01-11 22:44:05 +0900

    rename notificationsViewModel to settingsViewModel

--- app/src/main/java/com/j2rk/trendingifs/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/j2rk/trendingifs/ui/settings/SettingsFragment.kt
@@ -12,18 +12,18 @@ import com.j2rk.trendingifs.R
 
 class SettingsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: SettingsViewModel
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
+        settingsViewModel =
             ViewModelProvider(this).get(SettingsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
         val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
